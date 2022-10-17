07e4dc528eaff7896b95ef394cfac407ffb1e122
hitto <hitto@DESKTOP-A07E2BV>
2022-10-17 21:30:18 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 1; i < 10; i++)
+			{
+				int space = 10 - i;
+				int star = i;
+				string row = new string(' ', space) + new string('*', star);
+				Console.WriteLine(row);
+
+			}
+		}
+	}
+}
